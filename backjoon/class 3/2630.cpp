4e07077e0c55d2413a258d5393f4f04df9599fdc
Ddong-Min <110894447+Ddong-Min@users.noreply.github.com>
2025-03-22 03:51:20 +0000

    25.3.22

--- backjoon/class 3/2630.cpp
+++ backjoon/class 3/2630.cpp
@@ -13,7 +13,10 @@ void Topdown(int row, int column, int N){
             weight += square[i][j];
         }
     }
+    //정사각형 안에 값 다 더한게 N*N 이면 검정 정사각형 count
     if(weight == N*N) blue ++;
+
+    //정사각형 안에 값 다 더한게 0이면 흰 정사각형 count
     else if(weight == 0) white++;
     else{
         for(int i=0; i<4; i++){
