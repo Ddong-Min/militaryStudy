d7c2378c0d4bd529c35420f36297c7ab3caa80b8
Ddong-Min <110894447+Ddong-Min@users.noreply.github.com>
2025-06-02 10:14:15 +0000

    25.6.2

--- backjoon/class 4/12851.cpp
+++ backjoon/class 4/12851.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <algorithm>
+#include<queue>
+#include<vector>
 
 using namespace std;
+/*1번풀이 change visit value is true when the data is pushed into queue
+   If the value has already been visited, do not push it into the queue. 
+   However, if the current depth is equal to the previously computed minimal depth for that value, increment the count
 class Queue{
 private:
     int front=0;
@@ -62,7 +68,67 @@ int main() {
     cout << min << '\n';
     cout << cnt[K];
     return 0;
+}*/
+
+/*풀이2 : The first solution is poor in how it handles counting.
+It's more concise to mark a node as visited when it's popped from the queue, not when it's pushed,
+because pushing shouldn't depend on whether the node has already been visited.
+
+but this method is slower than first method
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N, K;
+    queue<pair<int, int>> q;
+
+    bool isVisit[100001];
+    fill(isVisit, isVisit+100001, false);
+    cin >> N >> K;
+
+    q.push(make_pair(N, 0));
+    isVisit[N]=true;
+    int count=0, time=100001;
+
+    while(!q.empty()){
+        auto now = q.front();
+        q.pop();
+        int value = now.first;
+        int depth = now.second;
+
+        isVisit[value]=true;
+        int next[3] = {value+1, value-1, value*2};
+        if(count == 0 && value == K){
+            count++;
+            time = depth;
+        }
+        else if(value == K && time == depth) count++;
+        if(time < depth) break;
+        for(int i=0; i<3; i++){ 
+            if(next[i] >= 0 && next[i] <= 100000 && !isVisit[next[i]]){
+                q.push(make_pair(next[i], depth+1));
+            }
+        }
+    }
+    cout << time << endl;
+    cout << count;
+    return 0;
 }
 
-//5 4 8 16 17
-//5 10 9 18 17
+*/
+
+
+//근데 지금 보니까 풀이 1이 복잡하기는 해도 더 효율적인 풀이같음
+/* 그이유는 일단 풀이2는 모든 경우를 다 확인해보지만, 풀이 1는 모든 경우를 다 확인하지는 않음
+이게 무슨말이냐 하면 
+만약 1->2->4 이런 경우와 3->2->4 이런 경우가 있다면 
+풀이 1 은 2까지 가는 방법이 2가지 따라서 4까지도 2가지 이런느낌이고
+
+풀이2는 4에 도달시 경우를 확인해보니 1->2->4가 있고, 3->2->4 가 있구나 라고 확인하는 경우
+
+따라서 풀이 1은 원하는 값에 도달할 경우 2->4 한번만 확인하면 되지만
+풀이 2는 4에 도달 후 두번 값을 확인함
+
+이게 값이 커지면 더 큰 시간 차이가 발생
+*/
